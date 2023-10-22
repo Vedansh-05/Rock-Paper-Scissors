aea7290b48e07e55af951fba66fd961ba51b5347
Vedansh-05 <110123121@nitt.edu>
2023-10-22 19:17:59 +0530

    Make the function playRound to play a single round of rock papers scissors

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection.toLowerCase()==="rock"){
+        if (computerSelection.toLowerCase()==="scissors"){
+            return "You win! Rock beats Scissors";
+        }
+        else if(computerSelection.toLowerCase()==="paper"){
+            return "You lose! Paper beats Rock";
+        }
+        else{
+            return "It's a tie!";
+        }
+    }
+    else if (playerSelection.toLowerCase()==="paper"){
+        if (computerSelection.toLowerCase()==="rock"){
+            return "You win! Paper beats Rock";
+        }
+        else if(computerSelection.toLowerCase()==="scissors"){
+            return "You lose! Scissors beats Papers";
+        }
+        else{
+            return "It's a tie!";
+        }
+    }
+    else if (playerSelection.toLowerCase()==="scissors"){
+        if (computerSelection.toLowerCase()==="paper"){
+            return "You win! Scissors beats Paper";
+        }
+        else if(computerSelection.toLowerCase()==="rock"){
+            return "You lose! Rock beats Scissors";
+        }
+        else{
+            return "It's a tie!";
+        }
+    }   
+}
